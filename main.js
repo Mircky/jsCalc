b0ea9bdda205aa9664ce5161a63f27a4562ee401
Mircky114 <frez114@yandex.ru>
2021-10-30 00:17:35 +0300

    done

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ const num_space = document.querySelector(".num_space");
 
 function clear() {
   const num_space = document.querySelector(".num_space");
-  num_space.textContent = "";
+  num_space.textContent ="";
 }
 
 const buttons = Array.from(document.querySelectorAll(".operator_buttons"));
-
 const clearButton = buttons[0];
-const sumButton = buttons[1];
-const minusButton = buttons[2];
-const multyButton = buttons[3];
-const devideButton = buttons[4];
-const evenButton = buttons[5];
-
 clearButton.addEventListener('click', clear);
+
+function input(i) {
+  if (num_space.textContent === '0')
+  {
+    num_space.textContent = ''
+    num_space.textContent = num_space.textContent+i;
+  }
+  else {
+    num_space.textContent = num_space.textContent+i;
+  }
+  // num_space.textContent = num_space.textContent+i;
+}
+
+function result() {
+  num_space.textContent = eval(num_space.textContent)
+}
