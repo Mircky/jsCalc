03645ceb41f0928ed5e8da174413ef3c1cc619cb
Mircky114 <frez114@yandex.ru>
2021-10-29 19:56:10 +0300

    variables

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ function clear() {
 const buttons = Array.from(document.querySelectorAll(".operator_buttons"));
 
 const clearButton = buttons[0];
+const sumButton = buttons[1];
+const minusButton = buttons[2];
+const multyButton = buttons[3];
+const devideButton = buttons[4];
+const evenButton = buttons[5];
+
 clearButton.addEventListener('click', clear);
